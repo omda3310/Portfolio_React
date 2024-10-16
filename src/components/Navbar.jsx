5d10bdcc68e3fbcf10780feb0e73516501231c3b
omda3310 <6197@holbertonstudents.com>
2024-10-16 16:41:06 +0200

    responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,45 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='bg-black text-white px-8 md:px-16 lg:px-24'>
-      <div className='container py-2 flex justify-center md:justify-between items-center'>
-        <div className='text-2xl font-bold hidden md:inline'>Imed</div>
-        <div className='space-x-6'>
+    <nav className='bg-black text-white px-4 sm:px-6 md:px-16 lg:px-24'>
+      <div className='container mx-auto py-3 flex flex-wrap items-center justify-between'>
+        <div className='w-full flex justify-between md:w-auto'>
+          <div className='text-2xl font-bold md:inline'>Imed</div>
+          <button className='text-white md:hidden'>
+            <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7'></path>
+            </svg>
+          </button>
+        </div>
+
+        <div className='hidden md:flex space-x-6'>
           <a href="#home" className='hover:text-gray-400'>Home</a>
-          <a href="#about" className='hover:text-gray-400'>About-Me</a>
+          <a href="#about" className='hover:text-gray-400'>About Me</a>
           <a href="#service" className='hover:text-gray-400'>Services</a>
           <a href="#project" className='hover:text-gray-400'>Projects</a>
           <a href="#contact" className='hover:text-gray-400'>Contact</a>
         </div>
-        <a href='#contact'>
-          <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
+
+        <a href='#contact' className='hidden md:inline-block'>
+          <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Connect Me</button>
         </a>
+      </div>
+
+      {/* Mobile menu */}
+      <div className='md:hidden'>
+        <div className='flex flex-col items-center space-y-2 py-4'>
+          <a href="#home" className='hover:text-gray-400'>Home</a>
+          <a href="#about" className='hover:text-gray-400'>About Me</a>
+          <a href="#service" className='hover:text-gray-400'>Services</a>
+          <a href="#project" className='hover:text-gray-400'>Projects</a>
+          <a href="#contact" className='hover:text-gray-400'>Contact</a>
+          <a href='#contact'>
+            <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+              Connect Me
+            </button>
+          </a>
+        </div>
       </div>
     </nav>
   )
